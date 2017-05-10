be8eec9891bd9a85808d98af367deaa42e79f674
RainbowLand <sherry250341@gmail.com>
2017-05-10 18:08:00 +0800

    add Travis CI files
    add Travis CI files

---
+++ install_depency.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+sudo apt-get install autoconf automake python-devel mysql-devel -qq -y
+cd third_party
+tar -xzf curl-7.53.1.tar.gz
+cd curl-7.53.1
+./configure && make install
+cd ..
+tar -xzf libevent-2.0.22-stable.tar.gz
+cd libevent-2.0.22-stable
+./configure && make install
+cd ..
+tar -xzf zlib-1.2.11.tar.gz
+cd zlib-1.2.11
+./configure && make install
+cd ..
+unzip libiconv-1.14.zip
+cd libiconv-1.14
+./configure && make install
+cd ..
+unzip mysqlpp.zip
+cd mysqlpp
+./configure && make install
+cd ..
