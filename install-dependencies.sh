267b1f46c0bf614f57bb1921daa9cca3b9d4756a
RainbowLand <sherry250341@gmail.com>
2017-05-17 18:04:54 +0800

    fix automake error

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,32 +1,33 @@
 #!/bin/bash
+lib_install_base_dir="/usr/local/"
 cd third_party
 tar -xzf curl-7.53.1.tar.gz
 cd curl-7.53.1
-./configure --prefix=/usr/local/curl && make -j5 && make install
-ln -s /usr/local/curl/lib/libcurl.so /usr/lib/libcurl.so
+./configure --prefix=$lib_install_base_dir"curl" && make -j5 && sudo make install
+ln -s $lib_install_base_dir"curl/lib/libcurl.so" /usr/lib/libcurl.so
 cd ..
 tar -xzf libevent-2.0.22-stable.tar.gz
 cd libevent-2.0.22-stable
-./configure --prefix=/usr/local/libevent2 && make -j5 && make install
-ln -s /usr/local/libevent2/lib/libevent.so /usr/lib/libevent.so
-ln -s /usr/local/libevent2/lib/libevent_pthreads.so /usr/lib/libevent_pthreads.so
-ln -s /usr/local/libevent2/lib/libevent_openssl.so /usr/lib/libevent_openssl.so
-ln -s /usr/local/libevent2/lib/libevent_extra.so /usr/lib/libevent_extra.so
-ln -s /usr/local/libevent2/lib/libevent_core.so /usr/lib/libevent_core.so
+./configure --prefix=$lib_install_base_dir"/libevent2" && make -j5 && sudo make install
+ln -s $lib_install_base_dir"libevent2/lib/libevent.so" /usr/lib/libevent.so
+ln -s $lib_install_base_dir"libevent2/lib/libevent_pthreads.so" "/usr/lib/libevent_pthreads.so"
+ln -s $lib_install_base_dir"libevent2/lib/libevent_openssl.so" "/usr/lib/libevent_openssl.so"
+ln -s $lib_install_base_dir"libevent2/lib/libevent_extra.so" "/usr/lib/libevent_extra.so"
+ln -s $lib_install_base_dir"libevent2/lib/libevent_core.so" "/usr/lib/libevent_core.so"
 cd ..
 tar -xzf zlib-1.2.11.tar.gz
 cd zlib-1.2.11
-./configure --prefix=/usr/local/zlib && make -j5 && make install
-ln -s /usr/local/zlib/lib/libz.so /usr/lib/libz.so
+./configure --prefix=$lib_install_base_dir"zlib" && make -j5 && sudo make install
+ln -s $lib_install_base_dir"zlib/lib/libz.so" "/usr/lib/libz.so"
 cd ..
 unzip libiconv-1.14.zip
 cd libiconv-1.14
-./configure --prefix=/usr/local/iconv && make -j5 && make install
-ln -s /usr/local/iconv/lib/libiconv.so /usr/lib/libiconv.so
+./configure --prefix=$lib_install_base_dir"iconv" && make -j5 && sudo make install
+ln -s $lib_install_base_dir"iconv/lib/libiconv.so" "/usr/lib/libiconv.so"
 cd ..
 unzip mysqlpp.zip
 cd mysqlpp
 chmod 755 configure
-./configure --prefix=/usr/local/mysqlpp && make -j5 && make install
-ln -s /usr/local/mysqlpp/lib/libmysqlpp.so /usr/lib/libmysqlpp.so
+./configure --prefix=$lib_install_base_dir"mysqlpp" && make -j5 && sudo make install
+ln -s $lib_install_base_dir"mysqlpp/lib/libmysqlpp.so" "/usr/lib/libmysqlpp.so"
 cd ..
