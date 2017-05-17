34003b02684ae4c92bf30b55df68962b72434f89
RainbowLand <sherry250341@gmail.com>
2017-05-17 17:13:06 +0800

    Fix configure error in some version of Linux
    Fix configure error in some version of Linux

--- configure.sh
+++ configure.sh
@@ -2,9 +2,7 @@
 base_dir=`pwd`
 echo $base_dir
 cd $base_dir"/src"
-aclocal && automake && autoconf
 chmod 755 configure
-make distclean
 mkdir -p linux_debug
 mkdir -p linux_release
 cd $base_dir"/src/linux_debug"
