1b4e3f125642dbed0944d21aef8b82a3c37fe3c7
RainbowLand <sherry250341@gmail.com>
2017-05-17 17:53:51 +0800

    fix automake error

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -2,21 +2,31 @@
 cd third_party
 tar -xzf curl-7.53.1.tar.gz
 cd curl-7.53.1
-./configure && make install
+./configure --prefix=/usr/local/curl && make -j5 && make install
+ln -s /usr/local/curl/lib/libcurl.so /usr/lib/libcurl.so
 cd ..
 tar -xzf libevent-2.0.22-stable.tar.gz
 cd libevent-2.0.22-stable
-./configure && make install
+./configure --prefix=/usr/local/libevent2 && make -j5 && make install
+ln -s /usr/local/libevent2/lib/libevent.so /usr/lib/libevent.so
+ln -s /usr/local/libevent2/lib/libevent_pthreads.so /usr/lib/libevent_pthreads.so
+ln -s /usr/local/libevent2/lib/libevent_openssl.so /usr/lib/libevent_openssl.so
+ln -s /usr/local/libevent2/lib/libevent_extra.so /usr/lib/libevent_extra.so
+ln -s /usr/local/libevent2/lib/libevent_core.so /usr/lib/libevent_core.so
 cd ..
 tar -xzf zlib-1.2.11.tar.gz
 cd zlib-1.2.11
-./configure && make install
+./configure --prefix=/usr/local/zlib && make -j5 && make install
+ln -s /usr/local/zlib/lib/libz.so /usr/lib/libz.so
 cd ..
 unzip libiconv-1.14.zip
 cd libiconv-1.14
-./configure && make install
+./configure --prefix=/usr/local/iconv && make -j5 && make install
+ln -s /usr/local/iconv/lib/libiconv.so /usr/lib/libiconv.so
 cd ..
 unzip mysqlpp.zip
 cd mysqlpp
-./configure && make install
+chmod 755 configure
+./configure --prefix=/usr/local/mysqlpp && make -j5 && make install
+ln -s /usr/local/mysqlpp/lib/libmysqlpp.so /usr/lib/libmysqlpp.so
 cd ..
