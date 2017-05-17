5482ec88f82fed5b4d38873b2fa2e86d9e0b1585
RainbowLand <sherry250341@gmail.com>
2017-05-17 17:23:07 +0800

    fix auto build error
    fix auto build error

--- configure.sh
+++ configure.sh
@@ -2,6 +2,7 @@
 base_dir=`pwd`
 echo $base_dir
 cd $base_dir"/src"
+aclocal && autoheader && autoreconf -f -i -Wall,no-obsolete && automake --add-missing && autoconf
 chmod 755 configure
 mkdir -p linux_debug
 mkdir -p linux_release
