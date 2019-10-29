bfda5af03a57032e12c4fa7307aefcd12ce8e394
RainbowLand <sherry0319@163.com>
2019-10-29 16:42:53 +0800

    1

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -3,19 +3,19 @@ base_dir=`pwd`"/third_party"
 cd $base_dir
 tar -xzf curl-7.53.1.tar.gz
 cd curl-7.53.1
-./configure && make install
+./configure && sudo make install
 cd $base_dir
 tar -xzf zlib-1.2.11.tar.gz
 cd zlib-1.2.11
-./configure && make install
+./configure && sudo make install
 cd $base_dir
 unzip libiconv-1.14.zip
 cd libiconv-1.14
 sudo chmod 755 configure
 ./configure && sudo make install
 cd $base_dir
-sudo ll /usr/lib64/ | grep mysql
-sudo ll /usr/lib/ | grep mysql
+sudo ls -l /usr/lib64/ | grep mysql
+sudo ls -l /usr/lib/ | grep mysql
 unzip mysqlpp.zip
 cd mysqlpp
 chmod 755 configure
