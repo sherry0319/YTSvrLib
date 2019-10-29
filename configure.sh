624c6c968a68ae431c3ba336930e5e3128df773f
RainbowLand <sherry0319@163.com>
2019-10-29 15:20:13 +0800

    update to v2.0.0 beta

--- configure.sh
+++ configure.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 base_dir=`pwd`
 echo $base_dir
-svn update
 cd $base_dir"/src"
 autoscan && aclocal && autoheader && autoreconf -f -i -Wall,no-obsolete && automake && autoconf
 chmod 755 configure
