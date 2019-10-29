6de193b81987569962dcd1a301346ae26a2a0f50
RainbowLand <sherry0319@163.com>
2019-10-29 15:49:07 +0800

    update to v2.0.0 beta

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
+base_dir=`pwd`
 cd third_party
 tar -xzf curl-7.53.1.tar.gz
 cd curl-7.53.1
 ./configure && make install
-cd ..
+cd $base_dir
 tar -xzf zlib-1.2.11.tar.gz
 cd zlib-1.2.11
 ./configure && make install
-cd ..
+cd $base_dir
 unzip libiconv-1.14.zip
 cd libiconv-1.14
 ./configure && make install
-cd ..
+cd $base_dir
 unzip mysqlpp.zip
 cd mysqlpp
 chmod 755 configure
 ./configure && make install
-cd ..
+cd $base_dir
 unzip cpp_redis.zip
 mkdir -p build
 cd build
 cmake ..
 make install
-cd ..
+cd $base_dir
 tar -xzf lz4.v1.8.3.tar.gz
 cd lz4-1.8.3
 make install
